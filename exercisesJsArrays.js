5e9b5304858d72414455034a6d569d55f936a311
Richard Nixon <rnixonaf@gmail.com>
2024-12-13 16:47:37 +0000

    f*cking arrays

--- exercisesJsArrays.js
+++ exercisesJsArrays.js
@@ -55,7 +55,47 @@ Starter code
 In this exercise we have an array seriesPending with some TV series, through this exercise we will be managing this data as well as another array we will create later.
 
 
-Add a new string element at the end of the seriesPending array. It can be any value of your choice!
+3- Add a new string element at the end of the seriesPending array. It can be any value of your choice!
+4- Write a loop that goes from 0 to the length of the seriesPending array. Inside the loop, use console.log()
+ on each element of the array and its index.
+5- Inside the same loop, add a conditional that will check if one of the series inside the array is 
+"Boolean 99". In case it is, console.log "Found it!"
+6- Declare and initialize a variable seriesWatching with an empty array.
+7-Remove the first element of the seriesPending array and add it at the end of seriesWatching array. You can use a variable to store the
+ removed element as seen before or try to do everything in the same line
+8-Use a console.log() to see the length of both arrays.
+9- We know that there are 4 elements left in seriesPending. So, write another loop that goes from 0 until it gets to 4.
+ the code inside should execute exactly 4 times
+10-Inside the loop, write the code that will remove the first element of 
+the seriesPending and add it to seriesWatching. same as step 7
+11 - Add another console.log() to verify the length of the arrays after the loop. 
+seriesPending should have 0 elements while seriesWatching should have 5.
+12 - Let’s now remove all the elements from seriesWatching. We could run another loop and use the .pop() 
+method, but let’s instead use the .splice() method to remove all the elements in the array.
+13
+
 */
 
 let seriesPending = ['Breaking Bug', 'Game of Drones', 'Boolean 99', 'Arrested Developer'];
+let seriesWatching = [];
+
+seriesPending.push('Mr Robot')
+
+for(let i = 0; i < seriesPending.length; i++){
+    const element = seriesPending[i];
+    console.log(`Serie:${i} : ${seriesPending[i]}`);
+    if (seriesPending[i] === 'Boolean 99') {
+        console.log('Found it!');
+    }
+}
+seriesWatching.push(seriesPending.shift()); 
+
+for(let i = 0; i = seriesPending.length; i--){
+    console.log(`running : ${[i]}`);
+    seriesWatching.push(seriesPending.shift()); 
+
+}
+seriesWatching.splice(0,5);
+console.log(seriesPending.length);
+console.log(seriesWatching.length);
+
