68fbfd3975d93bd67933773b5a47fb8940266146
Richard Nixon <rnixonaf@gmail.com>
2025-02-11 20:04:52 +0000

    DOM-Manipulation exercise

---
+++ DOM-Manipulation/index.js
@@ -0,0 +1,69 @@
+// ITERATION 1: Get the Node element with the main title and console.log it
+
+// ... your code here
+console.log('JS connected');
+let h1s = document.querySelector('h1')
+console.log(h1s);
+// => <h1>Fruits and veggies</h1>
+// ----------------------------------------------
+
+// ITERATION 2: Console.log the main title text
+
+// ... your code here
+
+
+// => Fruits and veggies
+// ----------------------------------------------
+
+// ITERATION 3: Get all the nodes with the fruit items into a NodeList. Console.log the NodeList
+
+// ... your code here
+
+
+// => NodeList(5) [li, li, li, li, li]
+// ----------------------------------------------
+
+// ITERATION 4: Get all the veggie items. Console log the text of each one
+// think about loops, innerText vs innerHTML
+
+// ... your code here
+
+
+// Broccoli 
+// Potato 
+// Avocado 
+// Spinach
+// Tomato
+// ----------------------------------------------
+
+// ITERATION 5: Change to uppercase all fruits with Odd Index. 1 & 3.
+// think about loops, index and modulo operator
+
+// ... your code here
+
+
+// => <li>GRAPES</li>
+// => <li>PLUM</li>
+// ----------------------------------------------
+
+// ITERATION 6. Remove all elements that start with the letter "A" or "T".
+// should remove <li>Avocado</li> and <li>Tomato</li> from the <ul>
+
+// ... your code here
+
+
+// => ... <li>Broccoli</li>
+// => ... <li>Potato</li>
+// => ... <li>Spinach</li>
+// ----------------------------------------------
+
+// ITERATION 7. Create a new li with text "Banana" and add it to the fruits list
+// think of how to `create` a new `element` and `append` it to the <ul>
+// alternatively, you can use 
+
+// ... your code here
+
+
+// => ...
+// => ... <li>Guava</li>
+// => ... <li>Banana</li>
